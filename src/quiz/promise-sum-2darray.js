dbf7a0b85087f39c49717221ee4290bbd2cdeaab
Yuanyuan Yang <1005842457@qq.com>
2024-03-06 13:21:20 -0500

    Update promise-sum-2darray.js

--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -3,3 +3,24 @@ const array2D = [
     [4, 5, 6],
     [7, 8, 9]
 ];
+
+const sumArray = (arr) => {
+    return arr.reduce((acc, cur) => acc + cur, 0);
+};
+
+const sum2DArrayConcurrently = (array2D) => {
+    const promises = array2D.map(row => {
+        // Wrap the sumArray operation in a Promise
+        return new Promise((resolve) => {
+            const sum = sumArray(row);
+            resolve(sum);
+        });
+    });
+
+    return Promise.all(promises)
+        .then(sums => sumArray(sums));
+};
+
+sum2DArrayConcurrently(array2D).then(total => {
+    console.log('Total Sum:', total);
+});
